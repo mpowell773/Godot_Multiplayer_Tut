f80e3034d351284e5e8b3afb6f35896a52bbe19b
Matt Powell <mpowell171@gmail.com>
2026-07-28 16:40:54 -0400

    add input authority for clients and synchronize movement to server

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,7 +1,15 @@
+class_name Player
 extends CharacterBody2D
+
+@onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
+
+var input_multiplayer_authority: int
+
+func _ready() -> void:
+	player_input_synchronizer_component.set_multiplayer_authority(input_multiplayer_authority)
+	set_process(is_multiplayer_authority())
 
 
 func _process(_delta: float) -> void:
-	var movement_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = movement_vector * 100
+	velocity = player_input_synchronizer_component.movement_vector * 100
 	move_and_slide()
--- main.gd
+++ main.gd
@@ -9,8 +9,9 @@ var player_scene: PackedScene = preload("uid://egtpvj3ddlhx")
 
 func _ready() -> void:
 	multiplayer_spawner.spawn_function = func(data):
-		var player := player_scene.instantiate()
+		var player := player_scene.instantiate() as Player
 		player.name = str(data.peer_id)
+		player.input_multiplayer_authority = data.peer_id
 		return player
 	
 	peer_ready.rpc_id(SERVER_ID)
@@ -18,6 +19,8 @@ func _ready() -> void:
 
 @rpc("any_peer", "call_local", "reliable")
 func peer_ready() -> void:
+	# remote_sender_id will also consider server remote due to "call_local" in RPC.
+	# Using "call_local" helps limit branching logic for client and server calls.
 	var sender_id :=  multiplayer.get_remote_sender_id()
 	multiplayer_spawner.spawn({ "peer_id": sender_id })
 	
