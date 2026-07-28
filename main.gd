98214753366e66dc6ad62a498646c99345bb958c
Matt Powell <mpowell171@gmail.com>
2026-07-28 11:27:08 -0400

    add basic scene replication across instances

--- main.gd
+++ main.gd
@@ -2,10 +2,22 @@ extends Node
 
 const SERVER_ID: int = 1
 
+var player_scene: PackedScene = preload("uid://egtpvj3ddlhx")
+
+@onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
+
+
 func _ready() -> void:
+	multiplayer_spawner.spawn_function = func(data):
+		var player := player_scene.instantiate()
+		player.name = str(data.peer_id)
+		return player
+	
 	peer_ready.rpc_id(SERVER_ID)
 
 
 @rpc("any_peer", "call_local", "reliable")
 func peer_ready() -> void:
-	print("peer %s ready" % multiplayer.get_remote_sender_id() )
+	var sender_id :=  multiplayer.get_remote_sender_id()
+	multiplayer_spawner.spawn({ "peer_id": sender_id })
+	
