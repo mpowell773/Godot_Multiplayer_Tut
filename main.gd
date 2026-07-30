5ae8cc051b7fed4424f5df1d8386af8bd522f210
Matt Powell <mpowell171@gmail.com>
2026-07-30 15:41:34 -0400

    Add arena to confine players

--- main.gd
+++ main.gd
@@ -6,6 +6,7 @@ var player_scene: PackedScene = preload("uid://egtpvj3ddlhx")
 var enemy_scene: PackedScene = preload("uid://dea55va7q7xfs")
 
 @onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
+@onready var player_spawn_position: Marker2D = $PlayerSpawnPosition
 
 
 func _ready() -> void:
@@ -13,13 +14,14 @@ func _ready() -> void:
 		var player := player_scene.instantiate() as Player
 		player.name = str(data.peer_id)
 		player.input_multiplayer_authority = data.peer_id
+		player.global_position = player_spawn_position.global_position
 		return player
 	
 	peer_ready.rpc_id(SERVER_ID)
 
 	if is_multiplayer_authority():
 		var enemy = enemy_scene.instantiate() as Node2D
-		enemy.global_position = Vector2.ONE * 100
+		enemy.global_position = Vector2.ONE * 200
 		add_child(enemy)
 
 @rpc("any_peer", "call_local", "reliable")
