8fd429b9ee141d2745e10727ff989b45bb00f528
Matt Powell <mpowell171@gmail.com>
2026-07-30 12:25:38 -0400

    Add interval to bullet spawning add despawn to bullet

--- entities/bullet/bullet.gd
+++ entities/bullet/bullet.gd
@@ -4,8 +4,14 @@ extends Node2D
 # SPEED is in pixels per second
 const SPEED: int = 600
 
+@onready var life_timer: Timer = $LifeTimer
+
 var direction: Vector2
 
+
+func _ready() -> void:
+	life_timer.timeout.connect(_on_life_timer_timeout)
+	
 
 func _process(delta: float) -> void:
 	global_position += direction * SPEED * delta
@@ -15,3 +21,8 @@ func start(_direction: Vector2) -> void:
 	direction = _direction
 	# align bullet with gun's orientation
 	rotation = direction.angle()
+
+
+func _on_life_timer_timeout() -> void:
+	if is_multiplayer_authority():
+		queue_free()
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
 @onready var weapon_root: Node2D = $WeaponRoot
+@onready var fire_rate_timer: Timer = $FireRateTimer
 
 var bullet_scene: PackedScene = preload("uid://cmsm71jq22qef")
-
 var input_multiplayer_authority: int
 
 
@@ -24,14 +24,18 @@ func _process(_delta: float) -> void:
 		move_and_slide()
 		
 		if player_input_synchronizer_component.is_attack_pressed:
-			create_bullet()
+			try_create_bullet()
 
 
-func create_bullet() -> void:
+func try_create_bullet() -> void:
+	if not fire_rate_timer.is_stopped():
+		# Timer is still running, do not create bullet
+		return
+	
 	var bullet := bullet_scene.instantiate() as Bullet
 	bullet.global_position = weapon_root.global_position
 	# One must use caution when calling functions before adding to the scene tree.
 	# In this case, it's safe, but this should be something to be considered.
 	bullet.start(player_input_synchronizer_component.aim_vector)
 	get_parent().add_child(bullet, true)
-	
+	fire_rate_timer.start()
