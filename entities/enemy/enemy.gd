2f9f78495878f7de64be80ec0cf5be3cc2e0978a
Matt Powell <mpowell171@gmail.com>
2026-07-30 21:56:08 -0400

    add health component and refactor enemy health to use new component

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-var current_health: int = 1
 var target_position: Vector2
 
 @onready var area_2d: Area2D = $Area2D
 @onready var target_acquisition_timer: Timer = $TargetAcquisitionTimer
+@onready var health_component: HealthComponent = $HealthComponent
 
 
 func _ready() -> void:
@@ -12,6 +12,7 @@ func _ready() -> void:
 	target_acquisition_timer.timeout.connect(_on_target_acquisition_timer_timeout)
 	
 	if is_multiplayer_authority():
+		health_component.died.connect(_on_died)
 		acquire_target()
 
 
@@ -22,9 +23,7 @@ func _process(_delta: float) -> void:
 
 
 func handle_hit() -> void:
-	current_health -= 1
-	if current_health <= 0:
-		queue_free()
+	health_component.damage(1)
 
 
 func acquire_target() -> void:
@@ -61,3 +60,7 @@ func _on_area_entered(other_area: Area2D) -> void:
 func _on_target_acquisition_timer_timeout() -> void:
 	if is_multiplayer_authority():
 		acquire_target()
+
+
+func _on_died() -> void:
+	queue_free()
