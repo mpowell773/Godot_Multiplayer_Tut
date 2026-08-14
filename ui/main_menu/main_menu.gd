6bfb5db00cd31e16616da2baef3d7163a78400e8
Matt Powell <mpowell171@gmail.com>
2026-08-13 23:48:54 -0400

    implement multiplayer menu scene with previous functionality

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,19 +1,18 @@
 extends Control
 
-const PORT: int = 3000
-
 var main_scene: PackedScene = preload("uid://cxgeu56nx8jw0")
 
 @onready var single_player_button: Button = $VBoxContainer/SinglePlayerButton
 @onready var multiplayer_button: Button = $VBoxContainer/MultiplayerButton
 @onready var quit_button: Button = $VBoxContainer/QuitButton
 
+@onready var multiplayer_menu_scene: PackedScene = load("uid://b7ek2hamlcwdo")
+
 
 func _ready() -> void:
 	single_player_button.pressed.connect(_on_single_player_button_pressed)
 	multiplayer_button.pressed.connect(_on_multiplayer_button_pressed)
 	quit_button.pressed.connect(_on_quit_button_pressed)
-	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
 
 #region Signals
@@ -23,28 +22,10 @@ func _on_single_player_button_pressed() -> void:
 
 
 func _on_multiplayer_button_pressed() -> void:
-	pass
+	get_tree().change_scene_to_packed(multiplayer_menu_scene)
 
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_host_pressed() -> void:
-	var server_peer := ENetMultiplayerPeer.new()
-	server_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = server_peer
-	get_tree().change_scene_to_packed(main_scene)
-
-
-func _on_join_pressed() -> void:
-	var client_peer := ENetMultiplayerPeer.new()
-	client_peer.create_client("127.0.0.1", PORT)
-	multiplayer.multiplayer_peer = client_peer
-
-
-func _on_connected_to_server() -> void:
-	get_tree().change_scene_to_packed(main_scene)
-
 
 #endregion
