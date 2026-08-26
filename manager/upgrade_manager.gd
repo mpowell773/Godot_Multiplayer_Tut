190a5b7b264a17cbe446c7421c795f08312a6813
Matt Powell <mpowell171@gmail.com>
2026-08-25 23:29:35 -0400

    synchronize upgrades spawning for each client

--- manager/upgrade_manager.gd
+++ manager/upgrade_manager.gd
@@ -13,7 +13,44 @@ func _ready() -> void:
 	enemy_manager.round_completed.connect(_on_round_completed)
 
 
+func generate_upgrade_options() -> void:
+	var connected_peer_ids := multiplayer.get_peers()
+	connected_peer_ids.append(MultiplayerPeer.TARGET_PEER_SERVER)
+	
+	for connected_peer_id in connected_peer_ids:
+		var selected_upgrades: Array[String] = [
+			available_upgrades[0].id,
+			available_upgrades[0].id,
+			available_upgrades[0].id
+		]
+		set_upgrade_options.rpc_id(connected_peer_id, selected_upgrades)
+
+
+func show_upgrade_resources(upgrade_resources: Array[UpgradeResource]) -> void:
+	var initial_x: int = -64
+	var x_difference: int = 64
+	
+	for i in range(upgrade_resources.size()):
+		var upgrade_option := upgrade_option_scene.instantiate() as UpgradeOption
+		upgrade_option.global_position = spawn_position.global_position
+		# Spreads 3 upgrades evenly across the arena.
+		upgrade_option.global_position += Vector2.RIGHT * (initial_x + (x_difference * i))
+		
+		spawn_root.add_child(upgrade_option)
+
+
+@rpc("authority", "call_local", "reliable")
+func set_upgrade_options(upgrade_ids: Array[String]) -> void:
+	var upgrade_resouces: Array[UpgradeResource] = []
+	
+	for upgrade_id in upgrade_ids:
+		var resource_index := available_upgrades.find_custom(func (item: UpgradeResource):
+			return item.id == upgrade_id
+		)
+		upgrade_resouces.append(available_upgrades[resource_index])
+	
+	show_upgrade_resources(upgrade_resouces)
+
+
 func _on_round_completed() -> void:
-	var upgrade_option := upgrade_option_scene.instantiate() as UpgradeOption
-	upgrade_option.global_position = spawn_position.global_position
-	spawn_root.add_child(upgrade_option)
+	generate_upgrade_options()
