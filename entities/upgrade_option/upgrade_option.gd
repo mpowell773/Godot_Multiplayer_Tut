bf2faeafdefe10bf0269ea9fcd69bede69c7ef21
Matt Powell <mpowell171@gmail.com>
2026-08-26 13:03:47 -0400

    add ability to choose upgrade

--- entities/upgrade_option/upgrade_option.gd
+++ entities/upgrade_option/upgrade_option.gd
@@ -1,2 +1,25 @@
 class_name UpgradeOption
 extends Node2D
+
+signal selected(index: int)
+
+var upgrade_index: int
+var assigned_resource: UpgradeResource
+
+@onready var health_component: HealthComponent = $HealthComponent
+
+
+func _ready() -> void:
+	health_component.died.connect(_on_died)
+
+
+func set_upgrade_index(index: int) -> void:
+	upgrade_index = index
+
+
+func set_upgrade_resource(upgrade_resource: UpgradeResource) -> void:
+	assigned_resource = upgrade_resource
+
+
+func _on_died() -> void:
+	selected.emit(upgrade_index)
--- manager/upgrade_manager.gd
+++ manager/upgrade_manager.gd
@@ -7,6 +7,7 @@ extends Node
 @export var available_upgrades: Array[UpgradeResource]
 
 var upgrade_option_scene: PackedScene = preload("uid://egb6it4cxmj6")
+var peer_id_to_upgrade_options: Dictionary[int, Array] = {}
 
 
 func _ready() -> void:
@@ -14,6 +15,7 @@ func _ready() -> void:
 
 
 func generate_upgrade_options() -> void:
+	peer_id_to_upgrade_options.clear()
 	var connected_peer_ids := multiplayer.get_peers()
 	connected_peer_ids.append(MultiplayerPeer.TARGET_PEER_SERVER)
 	
@@ -22,6 +24,11 @@ func generate_upgrade_options() -> void:
 			available_upgrades[0].id,
 			available_upgrades[0].id,
 			available_upgrades[0].id
+		]
+		peer_id_to_upgrade_options[connected_peer_id] = [
+			available_upgrades[0],
+			available_upgrades[0],
+			available_upgrades[0]
 		]
 		set_upgrade_options.rpc_id(connected_peer_id, selected_upgrades)
 
@@ -32,11 +39,16 @@ func show_upgrade_resources(upgrade_resources: Array[UpgradeResource]) -> void:
 	
 	for i in range(upgrade_resources.size()):
 		var upgrade_option := upgrade_option_scene.instantiate() as UpgradeOption
+		upgrade_option.set_upgrade_index(i)
+		upgrade_option.set_upgrade_resource(upgrade_resources[i])
+		
 		upgrade_option.global_position = spawn_position.global_position
 		# Spreads 3 upgrades evenly across the arena.
 		upgrade_option.global_position += Vector2.RIGHT * (initial_x + (x_difference * i))
 		
 		spawn_root.add_child(upgrade_option)
+		
+		upgrade_option.selected.connect(_on_upgrade_option_selected)
 
 
 @rpc("authority", "call_local", "reliable")
@@ -52,5 +64,21 @@ func set_upgrade_options(upgrade_ids: Array[String]) -> void:
 	show_upgrade_resources(upgrade_resouces)
 
 
+@rpc("any_peer", "call_local", "reliable")
+func notify_upgrade_selected(upgrade_index: int) -> void:
+	if not is_multiplayer_authority():
+		return
+	
+	var peer_id := multiplayer.get_remote_sender_id()
+	print("Peer %s has selected upgrade with id %s" %[
+		peer_id,
+		peer_id_to_upgrade_options[peer_id][upgrade_index].id
+	])
+
+
 func _on_round_completed() -> void:
 	generate_upgrade_options()
+
+
+func _on_upgrade_option_selected(upgrade_index: int) -> void:
+	notify_upgrade_selected.rpc_id(MultiplayerPeer.TARGET_PEER_SERVER, upgrade_index)
