7af89b84fc35504579edfdc75e4f73ddcdb98000
Matt Powell <mpowell171@gmail.com>
2026-09-08 22:47:05 -0400

    fix warning with static func in ui_audio_manager

--- autoload/ui_audio_manager.gd
+++ autoload/ui_audio_manager.gd
@@ -2,17 +2,16 @@ extends Node
 
 static var instance: UIAudioManager
 
-
-static func register_buttons(buttons: Array) -> void:
-	for button in buttons:
-		button.pressed.connect(instance._on_button_pressed)
-
-
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 
 func _ready() -> void:
 	instance = self
+
+
+func register_buttons(buttons: Array) -> void:
+	for button in buttons:
+		button.pressed.connect(instance._on_button_pressed)
 
 
 func _on_button_pressed() -> void:
