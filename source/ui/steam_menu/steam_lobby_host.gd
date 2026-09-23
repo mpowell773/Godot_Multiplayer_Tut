990ff13181c298c3f3465433bcad69ee68289937
Matt Powell <mpowell171@gmail.com>
2026-09-22 22:03:40 -0400

    finish implementing SteamLobbyHost

--- source/ui/steam_menu/steam_lobby_host.gd
+++ source/ui/steam_menu/steam_lobby_host.gd
@@ -13,7 +13,6 @@ func _ready() -> void:
 	create_button.pressed.connect(_on_create_button_pressed)
 	host_back_button.pressed.connect(_on_host_back_button_pressed)
 
-
 	Steam.lobby_created.connect(_on_lobby_created)
 
 
@@ -32,12 +31,14 @@ func _on_lobby_created(connect_status: Steam.Result, lobby_id: int) -> void:
 		Steamworks.lobby_id = lobby_id
 
 	var lobby_name := "%s's lobby" % Steamworks.steam_username
-	if not Steam.setLobbyData(Steamworks.lobby_id, "lobby_name", lobby_name):
+	var has_set_lobby_name := Steam.setLobbyData(Steamworks.lobby_id, "lobby_name", lobby_name)
+	if not has_set_lobby_name:
 		printerr("Failed to set lobby name")
 
 	var data_sets: PackedStringArray = data_list_line_edit.text.split(",", false)
 	for this_data in data_sets:
 		var data_key_value: PackedStringArray = this_data.split(":", false, 1)
 		if data_key_value.size() == 2:
-			if not Steam.setLobbyData(Steamworks.lobby_id, data_key_value[0], data_key_value[1]):
+			var has_set_data := Steam.setLobbyData(Steamworks.lobby_id, data_key_value[0], data_key_value[1])
+			if not has_set_data:
 				printerr("Failed to set lobby %s data [%s : %s]" % [Steamworks.lobby_id, data_key_value[0], data_key_value[1]])
